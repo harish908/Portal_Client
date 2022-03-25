70df71f15db96ed08df3bacf724fc025e40387ff
harish908 <45508152+harish908@users.noreply.github.com>
2022-03-25 21:00:43 +0530

    a

--- build/jenkins/deploy.groovy
+++ build/jenkins/deploy.groovy
@@ -5,8 +5,8 @@ pipeline{
         }
     }
     parameters{
-        string(name: "DOMAIN", defalultValue: "portal", description: "Portal Domain")
-        string(name: "SERVICE", defalultValue: "portal-client", description: "Portal Client Service")
+        string(name: 'DOMAIN', defalultValue: 'portal', description: 'Portal Domain')
+        string(name: 'SERVICE', defalultValue: 'portal-client', description: 'Portal Client Service')
         choice(name: "ECR_ACCOUNT_ID", choices: "859114173848", description: "ECR Account Id")
         choice(name: "ECR_REGION", choices: "ap-south-1", description: "ECR Region")
         gitParameter name: 'DEPLOY_BRANCH', description: 'Select a branch to deploy', type: 'PT_BRANCH', defalultValue: 'origin/master', listSize: '0', selectedValue: 'DEFAULT'

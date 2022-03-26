f2263ac0e3d9a0a4733b3d23483494fd0fcc8b8b
harish908 <45508152+harish908@users.noreply.github.com>
2022-03-26 21:26:34 +0530

    a

--- build/jenkins/deploy.groovy
+++ build/jenkins/deploy.groovy
@@ -1,9 +1,5 @@
 pipeline{
-    agent{
-        kubernetes{
-            yamlFile 'build/jenkins/pod.yaml'
-        }
-    }
+    agent none
     parameters{
         string(name: "DOMAIN", defaultValue: "portal", description: "Portal Domain")
         string(name: "SERVICE", defaultValue: "portal-client", description: "Portal Client Service")
@@ -16,29 +12,11 @@ pipeline{
         DEPLOY_TARGET_ACCOUNT   = '859114173848'
     }
     stages{
-        stage('checkout'){
-            steps{
-                script{
-                    echo "DEPLOY BRANCH: ${params.DEPLOY_BRANCH}"
-                    sh(script: "git checkout ${params.DEPLOY_BRANCH}")
-                }
-            }
-        }
-
-        stage('env'){
-            steps{
-                script{
-                    env.REVISION = sh(script: "git rev-parse -short HEAD", returnStdout: true).trim()
-                    env.ECR_PORTAL_IMAGE = "${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/${DOMAIN}/${SERVICE}:${REVISION}.${BUILD_NUMBER}"
-                }
-            }
-        }
-
-        stage('image'){
-            steps{
-                container('kaniko'){
-                    withAWS(role: "${DEPLOYMENT_ROLE}", roleAccount: "${ECR_ACCOUNT_ID}", region: "${ECR_REGION}")
-                    sh "/kaniko/executor -f Dockerfile -c `pwd` --skip-tls-verify --cache=false --destination=${ECR_PORTAL_IMAGE}"
+        stage('Front'){
+            agent{
+                docker{ image 'node:16-alpine' }
+                steps{
+                    sh 'node --version'
                 }
             }
         }

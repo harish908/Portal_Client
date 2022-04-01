77cd101aeec037643bf47c0afb28f18774faef8d
harish908 <45508152+harish908@users.noreply.github.com>
2022-04-01 08:17:50 +0530

    a

--- build/jenkins/deploy.groovy
+++ build/jenkins/deploy.groovy
@@ -1,4 +1,9 @@
 pipeline{
+    agent {
+        kubernetes {
+            yamlFile 'build/jenkins/pod.yaml'
+        }
+    }
     parameters{
         string(name: "DOMAIN", defaultValue: "portal", description: "Portal Domain")
         string(name: "SERVICE", defaultValue: "portal-client", description: "Portal Client Service")
@@ -17,23 +22,60 @@ pipeline{
                     echo "DEPLOY BRANCH: ${params.DEPLOY_BRANCH}"
                     sh(script: "git checkout ${params.DEPLOY_BRANCH}")
                     env.REVISION = sh(script: "git rev-parse -short HEAD", returnStdout: true).trim()
-                    env.ECR_PORTAL_IMAGE = "${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com"
+                    env.ECR_PORTAL_IMAGE = "${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/${DOMAIN}:latest"
                 }
             }
         }
 
         stage('image'){
             steps{
-                script{
-                    docker.withRegistry("https://${env.ECR_PORTAL_IMAGE}", "harish-aws-creds"){
-                        def myImage = docker.build("${env.DOMAIN}")
-                        myImage.push("latest")
+                container('kaniko'){
+                    withCredentials([aws(accessKeyVariable:'AWS_ACCESS_KEY_ID', credentialsId:'harish-aws-creds', secretKeyVariable:'AWS_SECRET_ACCESS_KEY')]){
+                        sh "/kaniko/executor -f Dockerfile -c `pwd` --skip-tls-verify --cache=true --destination=${ECR_PORTAL_IMAGE}"
                     }
                 }
             }
         }
     }
 }
+
+
+// pipeline{
+//     parameters{
+//         string(name: "DOMAIN", defaultValue: "portal", description: "Portal Domain")
+//         string(name: "SERVICE", defaultValue: "portal-client", description: "Portal Client Service")
+//         choice(name: "ECR_ACCOUNT_ID", choices: "859114173848", description: "ECR Account Id")
+//         choice(name: "ECR_REGION", choices: "ap-south-1", description: "ECR Region")
+//         gitParameter name: 'DEPLOY_BRANCH', description: 'Select a branch to deploy', type: 'PT_BRANCH', defaultValue: 'origin/master', listSize: '0', selectedValue: 'DEFAULT'
+//     }
+//     environment{
+//         DEPLOYMENT_ROLE         = "JenkinsDeploymentRole"
+//         DEPLOY_TARGET_ACCOUNT   = '859114173848'
+//     }
+//     stages{
+//         stage('checkout'){
+//             steps{
+//                 script{
+//                     echo "DEPLOY BRANCH: ${params.DEPLOY_BRANCH}"
+//                     sh(script: "git checkout ${params.DEPLOY_BRANCH}")
+//                     env.REVISION = sh(script: "git rev-parse -short HEAD", returnStdout: true).trim()
+//                     env.ECR_PORTAL_IMAGE = "${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com"
+//                 }
+//             }
+//         }
+
+//         stage('image'){
+//             steps{
+//                 script{
+//                     docker.withRegistry("https://${env.ECR_PORTAL_IMAGE}", "harish-aws-creds"){
+//                         def myImage = docker.build("${env.DOMAIN}")
+//                         myImage.push("latest")
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
 // pipeline{
